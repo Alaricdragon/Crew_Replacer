843a9dde156f5c529a229d2f974913cf8ff01d42
Alaricdragon <alaric.meservy@gmail.com>
2024-02-23 22:08:55 -0700

    V0.6.8

--- jars/src/data/scripts/combatabilityPatches/CrewReplacer_InitCombatabilityPatches.java
+++ jars/src/data/scripts/combatabilityPatches/CrewReplacer_InitCombatabilityPatches.java
@@ -5,22 +5,22 @@ import data.scripts.CrewReplacer_Log;
 import data.scripts.combatabilityPatches.normadicSurvivalPatches.CrewReplacer_normadicSurvival_init;
 
 public class CrewReplacer_InitCombatabilityPatches {
-    public static String[] modNames = {
-            "sun_nomadic_survival",
+    public static String[][] modNames = {
+            {"sun_nomadic_survival","sun_perilous_expanse"}
     };
     public static CrewReplacer_PatchBase[] patches = {
            new CrewReplacer_normadicSurvival_init(),
     };
     public static void onApplicationLoad() {
         for(int a = 0; a < modNames.length; a++) {
-            if (Global.getSettings().getModManager().isModEnabled(modNames[a])) {
-                CrewReplacer_Log.loging("Crew Replacer is attempting to add a compatibility patch for the mod '" + modNames[a] + "' ...", CrewReplacer_InitCombatabilityPatches.class, true);
-                patches[a].apply();
+            for (int b = 0; b < modNames[a].length; b++) {
+                if (Global.getSettings().getModManager().isModEnabled(modNames[a][b])) {
+                    CrewReplacer_Log.loging("Crew Replacer is attempting to add a compatibility patch for the mod '" + modNames[a] + "' ...", CrewReplacer_InitCombatabilityPatches.class, true);
+                    patches[a].apply();
+                }
             }
         }
     }
     public static void onGameLoad(boolean newGame) {
-        if(Global.getSettings().getModManager().isModEnabled(modNames[0])) {
-        }
     }
 }
